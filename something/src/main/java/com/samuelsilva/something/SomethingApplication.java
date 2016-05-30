edce16097d0190d319227b80ab2d898eeee5e5f9
Samuel Silva <samuel.silva@sigga.com.br>
2016-05-30 17:09:41 -0300

    Build config

--- something/src/main/java/com/samuelsilva/something/SomethingApplication.java
+++ something/src/main/java/com/samuelsilva/something/SomethingApplication.java
@@ -5,7 +5,10 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 /**
@@ -22,5 +25,14 @@ public class SomethingApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("en", "US"));
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter {
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/userregistration");
+		}
 	}
 }
