a1db0bb0931bf6ea232ba6261ecb7a5c07387d0a
Samuel Silva <samuel.silva@sigga.com.br>
2016-05-31 11:15:34 -0300

    messages.properties

--- something/src/main/java/com/samuelsilva/something/SomethingApplication.java
+++ something/src/main/java/com/samuelsilva/something/SomethingApplication.java
@@ -4,8 +4,10 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -34,5 +36,13 @@ public class SomethingApplication {
 		public void addViewControllers(ViewControllerRegistry registry) {
 			registry.addRedirectViewController("/", "/userregistration");
 		}
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
 	}
 }
