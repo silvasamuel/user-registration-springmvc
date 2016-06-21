b1db80e5dc285933927bf8f62d21c0791a56026c
Samuel Silva <samuel.silva@sigga.com.br>
2016-06-21 11:24:15 -0300

    Annotations

--- something/src/main/java/com/samuelsilva/something/repository/UserRepository.java
+++ something/src/main/java/com/samuelsilva/something/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.samuelsilva.something.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.samuelsilva.something.model.User;
 
@@ -10,6 +11,7 @@ import com.samuelsilva.something.model.User;
  * @author samuel.silva
  */
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	public List<User> findByNameContaining(String name);
